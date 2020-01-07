d4c173c70d7a0d9257d02456b3e8982592518a28
Bruno Vinicius Guimaraes Silva <brunogs@cit>
2020-01-07 09:04:26 -0300

    Alteração dos eventos

---
+++ src/Componentes/Multiplos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BoaTarde = () => <h1>
+Boa Tarde
+</h1>
+
+export const BoaNoite = () => <h1>Boa Noite</h1>
---
+++ src/Componentes/Primeiro.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => <div>
+    <label>Teste</label>
+</div>
